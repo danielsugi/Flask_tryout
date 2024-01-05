3b12541aea085b38abb4f7879ebf0a9b20427756
daniel.sugiharto <daniel.sugiharto@printforce.nl>
2024-01-05 10:15:20 +0100

    basic Flask code

--- flask_app.py
+++ flask_app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 @app.route('/')
 
 def home():
-    return "<p>Hello world!</p>" 
+    return "<p>Hello world! This is Daniel's site.</p>" 
 
 if __name__ == '__main__':
     app.run(port=5000, debug=False)
